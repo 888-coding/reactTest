2c74ef10758176d37320e9df36508fd357a16d9c
888-coding <pachelee@gmail.com>
2023-09-24 06:30:28 -0300

    Correcao, funcionando padrao

--- src/App.js
+++ src/App.js
@@ -1,2 +1,8 @@
-
-
+export default function App(){
+       return(
+              <div>
+                     <h1>Bem vindo ao sistema</h1>
+                     <h2>Sujeito Programador</h2>
+              </div>
+       )
+}
